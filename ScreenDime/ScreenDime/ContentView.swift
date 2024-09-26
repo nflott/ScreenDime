644e80aa51df02d959b53d4bdd0d5019147da111
Luke Currier <currier.l@northeastern.edu>
2024-09-26 15:15:52 -0400

    added xcode project

---
+++ ScreenDime/ScreenDime/ContentView.swift
@@ -0,0 +1,18 @@
+//
+//  ContentView.swift
+//  ScreenDime
+//
+//  Created by Luke Currier on 9/24/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, SwiftUI!")
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ ScreenDime/ScreenDime/ScreenDimeApp.swift
@@ -0,0 +1,17 @@
+//
+//  ScreenDimeApp.swift
+//  ScreenDime
+//
+//  Created by Luke Currier on 9/24/24.
+//
+
+import SwiftUI
+
+@main
+struct ScreenDimeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
