dbbbcb17e4f6a8577b178e284d8614b05c8293f1
Noah F <flottnoah@gmail.com>
2024-11-06 15:29:12 -0500

    testing of bets and groups

--- ScreenDime/ScreenDimeTesting/ScreenDimeBetTests.swift
+++ ScreenDime/ScreenDimeTesting/ScreenDimeBetTests.swift
@@ -24,5 +24,34 @@ final class ScreenDimeBetTests: XCTestCase {
         
         XCTAssertEqual(bet.name, name)
     }
+    
+    func testBetJoining() {
+        let testGroup = Group(name: "Test Group", members: [], bets: [])
+        let participant = User(name: "Test User", age: 23, phoneNumber: "0000000000", email: "user@user.com", invites: [], groups: [], bets: [])
+        var bet = Bet(name: "Test Bet", group: testGroup, metric: "Daily average", appTracking: "All apps", participants: [], stakes: "Dishes", startDate: Date(), endDate: Date())
+        
+        let message = bet.joinBet(user: participant)
+        
+        XCTAssertEqual(message, "\(participant.name) has joined the bet.")
+    }
 
+    func testUnsuccessfulBetJoining() {
+        let testGroup = Group(name: "Test Group", members: [], bets: [])
+        let participant = User(name: "Test User", age: 23, phoneNumber: "0000000000", email: "user@user.com", invites: [], groups: [], bets: [])
+        var bet = Bet(name: "Test Bet", group: testGroup, metric: "Daily average", appTracking: "All apps", participants: [], stakes: "Dishes", startDate: Date(), endDate: Date())
+        
+        let msg = bet.joinBet(user: participant)
+        
+        XCTAssertEqual(bet.joinBet(user: participant), "\(participant.name) is already a participant.")
+    }
+    
+    func testInactiveBetJoining() {
+        let testGroup = Group(name: "Test Group", members: [], bets: [])
+        let participant = User(name: "Test User", age: 23, phoneNumber: "0000000000", email: "user@user.com", invites: [], groups: [], bets: [])
+        var bet = Bet(name: "Test Bet", group: testGroup, metric: "Daily average", appTracking: "All apps", participants: [], stakes: "Dishes", startDate: Date(timeIntervalSince1970: 10000), endDate: Date().addingTimeInterval(10000))
+        
+        let message = bet.joinBet(user: participant)
+        
+        XCTAssertEqual(message, "Bet is currently active. You cannot join.")
+    }
 }
