887334822598571f04dd52a794e3acbd419b634e
Luke Currier <currier.l@northeastern.edu>
2024-11-13 11:50:29 -0500

    updated title

--- ScreenDime/ScreenDime/Views/HomeView.swift
+++ ScreenDime/ScreenDime/Views/HomeView.swift
@@ -16,54 +16,59 @@ struct HomeView: View {
     ]
     
     var body: some View {
-        VStack {
-            HStack {
-                Button(action: {
-                    showingSettings.toggle()
-                }) {
-                    Image(systemName: "gearshape.fill")
-                        .resizable()
-                        .scaledToFit()
-                        .frame(width: 30, height: 30)
-                        .foregroundColor(.white)
-                        .padding([.leading, .trailing], 10)
-                        .padding([.top, .bottom], 8)
+        ZStack {
+            VStack {
+                HStack {
+                    Button(action: {
+                        showingSettings.toggle()
+                    }) {
+                        Image(systemName: "gearshape.fill")
+                            .resizable()
+                            .scaledToFit()
+                            .frame(width: 40, height: 40)
+                            .foregroundColor(.white)
+                            .padding([.leading, .trailing], 10)
+                    }
+                    .padding()
+                    
+                    Spacer()
                 }
+                .padding([.top, .bottom], 10)
                 
-                Spacer()
+                TabView(selection: $selectedTab) {
+                    ForEach(Array(tabs.enumerated()), id: \.element.id) { index, tab in
+                        tab.view
+                            .tag(index)
+                    }
+                }
+                .tabViewStyle(PageTabViewStyle(indexDisplayMode: .never))
                 
+                HStack {
+                    ForEach(0..<tabs.count, id: \.self) { index in
+                        Circle()
+                            .fill(index == selectedTab ? Color.blue : Color.gray)
+                            .frame(width: 15, height: 15)
+                            .onTapGesture {
+                                selectedTab = index
+                            }
+                    }
+                }
+            }
+            .applyBackground()
+            .sheet(isPresented: $showingSettings) {
+                SettingsView()
+            }
+            
+            VStack{
                 Text(tabs[selectedTab].title)
-                    .font(.title)
+                    .font(.largeTitle)
                     .foregroundColor(.white)
                     .fontWeight(.bold)
                     .frame(maxWidth: .infinity, alignment: .center)
+                    .padding([.top], 26)
                 
                 Spacer()
             }
-            .padding([.top, .bottom], 10)
-
-            TabView(selection: $selectedTab) {
-                ForEach(Array(tabs.enumerated()), id: \.element.id) { index, tab in
-                    tab.view
-                        .tag(index)
-                }
-            }
-            .tabViewStyle(PageTabViewStyle(indexDisplayMode: .never))
-            
-            HStack {
-                ForEach(0..<tabs.count, id: \.self) { index in
-                    Circle()
-                        .fill(index == selectedTab ? Color.blue : Color.gray)
-                        .frame(width: 15, height: 15)
-                        .onTapGesture {
-                            selectedTab = index
-                        }
-                }
-            }
-        }
-        .applyBackground()
-        .sheet(isPresented: $showingSettings) {
-            SettingsView()
         }
     }
 }
